56c68b995f2f6ce93b7f12318a11b3b8e4a2baf0
Stephen Wright <wrightsah@icloud.com>
2021-04-13 19:55:49 -0400

    rendered full array of employees

--- src/App.js
+++ src/App.js
@@ -10,21 +10,33 @@ import employees from "./employees.json";
 function App() {
   // ? is it working?
   console.log(employees.results[0].name.first);
-  console.log(employees.results[0].picture.medium);
+  console.log(employees.results[0].picture.large);
   return (
     <Wrapper>
       <Title>Employee Directory</Title>
       {/* This component is only for development */}
       <ExampleCard />
-
+      {/* This component displays one employee */}
       <EmployeeCard
-        picture={employees.results[0].picture.medium}
+        picture={employees.results[0].picture.large}
         firstName={employees.results[0].name.first}
         lastName={employees.results[0].name.last}
         location={employees.results[0].location.country}
         email={employees.results[0].email}
         phone={employees.results[0].phone}
       />
+
+      {/* Loop through entire employees array */}
+      {employees.results.map((employee) => (
+        <EmployeeCard
+          picture={employee.picture.large}
+          firstName={employee.name.first}
+          lastName={employee.name.last}
+          location={employee.location.country}
+          email={employee.email}
+          phone={employee.phone}
+        />
+      ))}
     </Wrapper>
   );
 }
