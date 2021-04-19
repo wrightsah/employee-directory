36c2b1a1f13f2fc312b8a7a05d5bf42bc0a92813
Stephen Wright <wrightsah@icloud.com>
2021-04-19 13:22:03 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
 import "./App.css";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
+import NavbarComponent from "./components/NavbarComponent"
 
 import EmployeeCard from "./components/EmployeeCard/EmployeeCard";
 import ExampleCard from "./components/ExampleCard/ExampleCard";
+
 
 import employees from "./employees.json";
 
 function App() {
   // ? is it working?
-  console.log(employees.results[0].name.first);
-  console.log(employees.results[0].picture.large);
+  // console.log(employees.results[0].name.first);
+  // console.log(employees.results[0].picture.large);
   return (
+    
     <Wrapper>
-      <Title>Employee Directory</Title>
+      <NavbarComponent />
+      {/* <Title>Employee Directory</Title> */}
       {/* This component is only for development */}
-      <ExampleCard />
+      {/* <ExampleCard /> */}
       {/* This component displays one employee */}
-      <EmployeeCard
+      {/* <EmployeeCard
         picture={employees.results[0].picture.large}
         firstName={employees.results[0].name.first}
         lastName={employees.results[0].name.last}
         location={employees.results[0].location.country}
         email={employees.results[0].email}
         phone={employees.results[0].phone}
-      />
+      /> */}
 
       {/* Loop through entire employees array */}
       {employees.results.map((employee) => (
--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,16 +1,25 @@
 // This component for Navbar
-import React from 'react';
+import React from "react";
 
-function NavbarComponent(){
+function NavbarComponent() {
+  return (
     <div>
-    <nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand">Navbar</a>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
-</nav>
-</div>;
+      <nav class="navbar navbar-light bg-light" expand="large">
+        <a class="navbar-brand">Employee Directory</a>
+        <form class="form-inline">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">
+            Search by Location
+          </button>
+        </form>
+      </nav>
+    </div>
+  );
 }
 
 export default NavbarComponent;
